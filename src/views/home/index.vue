fe7e87e80a3b6096ba87a89d6f2198df15bf9ea8
qinwencheng <157475034@qq.com>
2022-07-13 01:07:15 +0800

    feat: 初始化布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <span class="w-full">1</span>
-  <span class="w-full">1</span>
-  <div class="demo-input-suffix">
-    <el-row :gutter="20">
-      <span class="ml-3 w-35 text-gray-600 inline-flex items-center">Using attributes</span>
-      <el-input v-model="input1" class="w-50 m-2" placeholder="Pick a date" />
-      <el-input v-model="input2" class="w-50 m-2" placeholder="Type something" />
-    </el-row>
+  <div class="common-layout">
+    <el-container>
+      <el-aside>Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const input1 = ref('')
-const input2 = ref('')
 </script>
