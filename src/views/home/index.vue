566352274694cc27cebd640986cd4c76e53ae7c9
qinwencheng <157475034@qq.com>
2022-07-14 01:47:36 +0800

    feat: 增加左侧菜单栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="common-layout">
+
+  <el-container class="h-full border border-solid border-red-700">
+    <el-aside>
+      <h5 class="mb-2">Default colors</h5>
+      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <el-sub-menu index="1">
+          <template #title>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item one</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+
+    </el-aside>
     <el-container>
-      <el-aside>Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>
+        <TheFooter></TheFooter>
+      </el-footer>
     </el-container>
-  </div>
+  </el-container>
+
 </template>
 
 <script setup lang="ts">
