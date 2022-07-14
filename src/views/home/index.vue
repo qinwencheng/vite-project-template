849f24eea3cc66bd9b8651f273e9fe3fc5645f48
qinwencheng <157475034@qq.com>
2022-07-15 01:22:54 +0800

    refactor: 布局拆分成单独的组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,38 +2,12 @@
 
   <el-container class="h-full border border-solid border-red-700">
     <el-aside>
-      <h5 class="mb-2">Default colors</h5>
-      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-        <el-sub-menu index="1">
-          <template #title>
-            <span>Navigator One</span>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item one</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="2">
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <span>Navigator Four</span>
-        </el-menu-item>
-      </el-menu>
-
+      <Sider></Sider>
     </el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <Header></Header>
+      </el-header>
       <el-main>Main</el-main>
       <el-footer>
         <TheFooter></TheFooter>
@@ -44,4 +18,6 @@
 </template>
 
 <script setup lang="ts">
+import Header from "../layout/header/header.vue";
+import Sider from "../layout/sider/sider.vue";
 </script>
