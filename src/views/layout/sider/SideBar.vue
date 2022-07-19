9b0b9aa53a94394d210406e60da248a196547d74
qinwencheng <157475034@qq.com>
2022-07-20 00:18:52 +0800

    feat: 利用路由数组中的数据动态生成菜单item

--- src/views/layout/sider/SideBar.vue
+++ src/views/layout/sider/SideBar.vue
@@ -2,23 +2,18 @@
   <h5 class="mb-2" @click="ck">开关</h5>
   <el-scrollbar view-class="h-full">
     <el-menu :collapse="isCollapse" class="h-full" router @open="handleOpen" @close="handleClose">
-      <el-menu-item index="/test1">
-        <span>Navigator One</span>
-      </el-menu-item>
-      <el-menu-item index="/test2">
-        <span>Navigator Two</span>
-      </el-menu-item>
-      <el-menu-item index="/test3">
-        <span>Navigator Three</span>
-      </el-menu-item>
-      <el-menu-item index="/test4">
-        <span>Navigator Four</span>
-      </el-menu-item>
+      <template v-for="item in routerList">
+        <el-menu-item :index="item.path">
+          <span>{{ item.name }}</span>
+        </el-menu-item>
+      </template>
     </el-menu>
   </el-scrollbar>
 </template>
 
 <script setup lang="ts">
+import router from '@/router';
+
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
@@ -29,6 +24,7 @@ const [isCollapse, toggle] = useToggle();
 const ck = () => {
   toggle();
 };
+const routerList = router.getRoutes();
 </script>
 
 <style scoped></style>
