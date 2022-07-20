6b23e561cf9da87d58fed30b74555e3bdc8d1038
qinwencheng <157475034@qq.com>
2022-07-21 00:55:12 +0800

    refactor: 整理main.ts文件

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
-// i18n
-import { createI18n } from 'vue-i18n';
-import messages from '@intlify/vite-plugin-vue-i18n/messages';
+import { createApp } from 'vue';
+// element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 // vue router
 import router from '@/router/index';
 // pinia
 import store from '@/store';
-import App from './App.vue';
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// windicss
 import 'virtual:windi.css';
-
 // Devtools: https://windicss.org/integrations/vite.html#design-in-devtools
 import 'virtual:windi-devtools';
+
 import '@/assets/styles/index.scss';
+
+import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(ElementPlus)
-app.use(router).use(store);
+app.use(router).use(store).use(ElementPlus);
 
 app.mount('#app');
